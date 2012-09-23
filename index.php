338c8eb06bc9a43039e4a21c8ce8331e5c667ece
Aboubakr Seddik Ouahabi <ouahabix@gmail.com>
2012-09-23 01:20:59 +0100

    Baby steps

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/*********************************************************
+*	Author: Aboubakr Seddik Ouahabi (ouahabix/codernix)
+*	Email: ouahabix@gmail.com
+*	License: Please check LICENSE.txt
+*********************************************************/
+
+//This might be needed in some cases or some setups.
+//ob_start();
+
+include_once 'includes/Global.php';
+
+$Core->startApp();
+
+//ob_flush();
+?>
