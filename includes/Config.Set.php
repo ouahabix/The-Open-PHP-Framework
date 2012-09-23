6b6172c88f4420a6f5a2a56f9fdc503f51bdf41c
Aboubakr Seddik Ouahabi <ouahabix@gmail.com>
2012-09-23 01:27:28 +0100

    Baby steps

---
+++ includes/Config.Set.php
@@ -0,0 +1,12 @@
+<?php
+/*********************************************************
+*	Author: Aboubakr Seddik Ouahabi (ouahabix/codernix)
+*	Email: ouahabix@gmail.com
+*	License: Please check LICENSE.txt
+*********************************************************/
+
+$DB_HOST = 'localhost';			//Change this if it's not "localhost"
+$DB_NAME = 'YOUR_DB_NAME';
+$DB_USER = 'YOUR_DB_USERNAME';
+$DB_PASS = 'YOUR_DB_PASSWORD';
+?>
